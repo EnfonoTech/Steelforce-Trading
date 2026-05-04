5a349101d24762edef1aee33d318a2faee6ecff0
roshmol <roshnatnbr@gmail.com>
2026-05-04 13:24:14 +0000

    fate:crate branch configuration

---
+++ sf_trading/sf_trading/doctype/branch_configuration_cost_center/branch_configuration_cost_center.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, enfono and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Branch Configuration Cost Center", {
+// 	refresh(frm) {
+
+// 	},
+// });
